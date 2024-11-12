32aa2527cc11acf3b5c9bcf83f032029ac324530
yLexter <lucasmaia5954@gmail.com>
2024-11-12 18:56:52 -0300

    mais dados no json

--- Teste.py
+++ Teste.py
@@ -5,12 +5,5 @@ import os
 from apache_atlas.utils.Constants import EndRelations, TypeNames
 from apache_atlas.utils.API import API, HTTPMethod
 
-atlas_section = ApacheAtlasClient(
-    "http://localhost:21000",
-    "admin",
-    "admin"
-)
-
-random_int = random.randint(0, 100_000_000)
 
 
